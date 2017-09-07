13b84b787d6a46e440d7932e2794484fbe7d9567
Freek Van der Herten <freek@spatie.be>
2017-09-07 19:43:37 +0200

    Apply fixes from StyleCI (#44)

--- tests/__snapshots__/ArrayToXmlTest__and_cdata_values_can_also_be_set_in_simplexmlelement_style__1.php
+++ tests/__snapshots__/ArrayToXmlTest__and_cdata_values_can_also_be_set_in_simplexmlelement_style__1.php
@@ -1,3 +1,5 @@
-<?php return '<?xml version="1.0"?>
+<?php
+
+return '<?xml version="1.0"?>
 <root><movie><title category="SF"><![CDATA[<p>STAR WARS</p>]]></title></movie><movie><title category="Children"><![CDATA[<p>tom & jerry</p>]]></title></movie></root>
 ';
