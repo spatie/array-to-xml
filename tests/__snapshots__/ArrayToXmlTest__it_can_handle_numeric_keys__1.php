2e7eb9d15d5bd3e121e4ee7d021b6eca9ce6e307
Yoan Ballesteros <b.yoan@live.fr>
2019-08-21 08:32:00 +0200

    Fix XML structure when using numeric keys (#115)
    * Fix XML structure when using numeric keys
    
    * Apply fixes from StyleCI

--- tests/__snapshots__/ArrayToXmlTest__it_can_handle_numeric_keys__1.php
+++ tests/__snapshots__/ArrayToXmlTest__it_can_handle_numeric_keys__1.php
@@ -1,5 +1,5 @@
 <?php
 
 return '<?xml version="1.0"?>
-<root><numeric_16><parent>aaa</parent><numLinks>3</numLinks><child><parent>abc</parent><numLinks>3</numLinks></child></numeric_16><numeric_16><parent>bb</parent><numLinks>3</numLinks><child><parent>abb</parent><numLinks>3</numLinks><child><parent>abc</parent><numLinks>3</numLinks></child></child><child><parent>acb</parent><numLinks>3</numLinks></child></numeric_16><numeric_17><parent>aaa</parent><numLinks>3</numLinks><child><parent>abc</parent><numLinks>3</numLinks></child></numeric_17><numeric_17><parent>bb</parent><numLinks>3</numLinks><child><parent>abb</parent><numLinks>3</numLinks><child><parent>abc</parent><numLinks>3</numLinks></child></child><child><parent>acb</parent><numLinks>3</numLinks></child></numeric_17></root>
+<root><numeric_16><parent>aaa</parent><numLinks>3</numLinks><child><parent>abc</parent><numLinks>3</numLinks></child></numeric_16><numeric_17><parent>bb</parent><numLinks>3</numLinks><child><parent>abb</parent><numLinks>3</numLinks><child><parent>abc</parent><numLinks>3</numLinks></child></child><child><parent>acb</parent><numLinks>3</numLinks></child></numeric_17></root>
 ';
