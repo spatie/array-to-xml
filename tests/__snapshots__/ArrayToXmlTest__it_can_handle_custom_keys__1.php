ffbcf9eb7f7c776fdfd0a4fb3326474206f1245b
Freek Van der Herten <freek@spatie.be>
2020-08-22 10:46:44 +0200

    Apply fixes from StyleCI (#142)
    Co-authored-by: Freek Van der Herten <freekmurze@users.noreply.github.com>

--- tests/__snapshots__/ArrayToXmlTest__it_can_handle_custom_keys__1.php
+++ tests/__snapshots__/ArrayToXmlTest__it_can_handle_custom_keys__1.php
@@ -1,3 +1,5 @@
-<?php return '<?xml version="1.0"?>
+<?php
+
+return '<?xml version="1.0"?>
 <root><custom-key><parent>aaa</parent><numLinks>3</numLinks><child><parent>abc</parent><numLinks>3</numLinks></child></custom-key><custom-key><parent>bb</parent><numLinks>3</numLinks><child><custom-subkey><parent>abb</parent><numLinks>3</numLinks><child><custom-subsubkey><parent>abc</parent><numLinks>3</numLinks></custom-subsubkey></child></custom-subkey><custom-subkey><parent>acb</parent><numLinks>3</numLinks></custom-subkey></child></custom-key></root>
 ';
