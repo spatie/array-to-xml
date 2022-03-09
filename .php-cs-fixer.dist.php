16f9662662733b221665c63429320b2e7f1d20a8
otsch <otsch@users.noreply.github.com>
2022-03-09 23:27:49 +0100

    Rewrite phpunit tests to pest
    Also fix problems because null as second argument for DOMDocument and
    as argument for htmlspecialchars is deprecated. Further update PHP CS
    Fixer setup.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,9 @@
 <?php
 
-$finder = Symfony\Component\Finder\Finder::create()
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
     ->in([
         __DIR__ . '/src',
         __DIR__ . '/tests',
@@ -9,15 +12,16 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
+$config = new Config();
 
-return PhpCsFixer\Config::create()
+return $config->setFinder($finder)
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
         'binary_operator_spaces' => true,
@@ -28,7 +32,7 @@ return PhpCsFixer\Config::create()
         'phpdoc_var_without_name' => true,
         'class_attributes_separation' => [
             'elements' => [
-                'method',
+                'method' => 'one',
             ],
         ],
         'method_argument_space' => [
@@ -37,4 +41,4 @@ return PhpCsFixer\Config::create()
         ],
         'single_trait_insert_per_statement' => true,
     ])
-    ->setFinder($finder);
+    ->setRiskyAllowed(true);
