84a404e5b67dd21466a0ff47d335129d67b94029
freekmurze <freekmurze@users.noreply.github.com>
2023-11-08 08:19:46 +0000

    Fix styling

--- src/ArrayToXml.php
+++ src/ArrayToXml.php
@@ -212,7 +212,7 @@ class ArrayToXml
     protected function addNodeTypeAttribute(DOMElement $element, mixed $value): void
     {
         if (is_null($value)) {
-            if (!$this->rootNode->hasAttribute('xmlns:xsi')) {
+            if (! $this->rootNode->hasAttribute('xmlns:xsi')) {
                 $this->rootNode->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
             }
 
